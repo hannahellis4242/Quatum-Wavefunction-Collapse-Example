249ad74203350e015c90f7f39e946eb5b386ef87
hannahellis4242 <hannahellis4242@gmail.com>
2022-08-20 23:59:52 +0100

    generating png versions

--- img/convert.sh
+++ img/convert.sh
@@ -2,5 +2,6 @@ for f in *.svg
 do
     name=${f%.*}
     echo $name
-    convert $name.svg $name-128.png --size 128x128
+    #inkscape -w 128 -h 128 $name.svg -o $name-128.png
+    inkscape -z -w 128 -h 128 $name.svg -e $name.png
 done
